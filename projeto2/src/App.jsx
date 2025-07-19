051a89f2fadc7b8d56878358d80508208b5531bb
Eduardo Henrique <dudu30rafa@gmail.com>
2025-07-19 11:04:02 +0100

    Refactor to use React Router and add page components
    Replaced the Title component with React Router-based routing. Added Home, Sobre, and Contato pages, each with a shared Menu component. Removed unused CSS and Title component files. Updated dependencies to include react-router-dom.

--- projeto2/src/App.jsx
+++ projeto2/src/App.jsx
@@ -1,14 +1,20 @@
-import Title from "./title"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
 
 function App() {
   return (
     <div>
-      <Title color="blue" paragraph={false} />
-      <Title paragraph={true} />
-      <Title color="red" />
-      <Title />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  ) 
+  )
 }
 
 export default App
